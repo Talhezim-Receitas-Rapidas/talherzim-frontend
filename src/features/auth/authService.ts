990d838e582e0b1a2b212888de640b6fb785e647
miguelalchaar <miguel2alchaar@gmail.com>
2026-09-20 12:16:30 -0300

    feat: adiciona serviço de cadastro

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -13,3 +13,23 @@ export async function login(payload: LoginPayload): Promise<LoginResponse> {
   const { data } = await httpClient.post<LoginResponse>('/auth/login', payload);
   return data;
 }
+
+export interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  token: string;
+}
+
+export async function registrar(
+  payload: RegisterPayload,
+): Promise<RegisterResponse> {
+  const { data } = await httpClient.post<RegisterResponse>(
+    '/auth/registrar',
+    payload,
+  );
+  return data;
+}
